12f06f46513e8df85183f86683ad498be602a93f
Zan <nurdinizzan@gmail.com>
2022-01-19 10:45:19 +0700

    fix: remove unused variables

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,22 +1,10 @@
 import clsx from 'clsx';
 // import { LinkGetProps, WindowLocation } from '@reach/router';
-import { MenuNode, TocItem, Edge } from 'interfaces/nodes';
 import { Color, themeProps, Space } from '../components/Theme';
 
 export const getColor = (colorKey: Color) => themeProps.colors[colorKey];
 
 export const getSpacing = (spaceKey: Space) => themeProps.space[spaceKey];
-
-export const getPageById = (sectionList: Edge<MenuNode>[], templateFile?: string) => {
-  if (!templateFile) {
-    return undefined;
-  }
-
-  const sectionItems = sectionList.map(({ node }) => node.items);
-  const flattenedSectionItems: TocItem[] = ([] as TocItem[]).concat(...sectionItems);
-
-  return flattenedSectionItems.find((item) => item.id === templateFile);
-};
 
 function isDocsPath(location: any) {
   const { pathname } = location;
