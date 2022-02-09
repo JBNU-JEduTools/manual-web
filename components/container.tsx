d4caf73b6b5290b137866fe924e06c3e5347259b
Steven Evan <steven.evan999@gmail.com>
2022-02-09 07:48:44 +0700

    fix: change background color container

--- components/container.tsx
+++ components/container.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@aksara-ui/react";
+import { Box, theme } from '@aksara-ui/react';
 
 interface ContainerProps {
   children: React.ReactNode;
@@ -6,10 +6,16 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <Box display="flex" flexDirection="column" flex="1 1 auto" position="relative">
+    <Box
+      display="flex"
+      flexDirection="column"
+      flex="1 1 auto"
+      position="relative"
+      backgroundColor={theme.colors.greylight02}
+    >
       {children}
     </Box>
-  )
-}
+  );
+};
 
-export default Container
+export default Container;
