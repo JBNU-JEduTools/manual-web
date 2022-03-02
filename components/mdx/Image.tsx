6bd1d71e2275b6edac608243e9ad19a563e79081
Steven Evan <steven.evan999@gmail.com>
2022-03-02 09:54:36 +0700

    feat: add automated calculation image size

--- components/mdx/Image.tsx
+++ components/mdx/Image.tsx
@@ -1,5 +1,11 @@
 import { Box, theme, Text } from '@aksara-ui/react';
 import Image from 'next/image';
+import React from 'react';
+
+export const ImageWrapper = (props) => {
+  const { className, ...rest } = props;
+  return <Box className={className} marginY={24} textAlign="center" position={'relative'} {...rest} />;
+};
 
 const CaptionWrapper = (props) => {
   const { className, ...rest } = props;
@@ -18,51 +24,72 @@ const CaptionWrapper = (props) => {
   );
 };
 
+const MAX_PORTRAIT_HEIGHT = 720;
+const MAX_LANDSCAPE_WIDTH = 720;
+
+const calculateImageDimension = ({ naturalHeight, naturalWidth }) => {
+  // If layout is other than fill, onLoadingComplete will return small size for naturalHeight and naturalWidth
+  if (naturalHeight === 0 || naturalWidth === 0) return { height: 0, width: 0, layout: 'fill' };
+
+  if (naturalHeight > naturalWidth) {
+    if (naturalHeight <= MAX_PORTRAIT_HEIGHT) {
+      return {
+        height: naturalHeight,
+        width: naturalWidth > MAX_LANDSCAPE_WIDTH ? MAX_LANDSCAPE_WIDTH : naturalWidth,
+        layout: 'intrinsic',
+      };
+    } else {
+      return {
+        height: MAX_PORTRAIT_HEIGHT,
+        width: Math.floor((MAX_PORTRAIT_HEIGHT / naturalHeight) * naturalWidth),
+        layout: 'intrinsic',
+      };
+    }
+  } else {
+    if (naturalWidth <= MAX_LANDSCAPE_WIDTH) {
+      return {
+        height: naturalHeight > MAX_PORTRAIT_HEIGHT ? MAX_PORTRAIT_HEIGHT : naturalHeight,
+        width: naturalWidth,
+        layout: 'intrinsic',
+      };
+    } else {
+      // Resize using max width if larger than threshold
+      return {
+        height: Math.floor((MAX_LANDSCAPE_WIDTH / naturalWidth) * naturalHeight),
+        width: MAX_LANDSCAPE_WIDTH,
+        layout: 'responsive',
+      };
+    }
+  }
+};
+
+export const NextImage = (props) => {
+  const { src, alt, ...rest } = props;
+  const [naturalDimension, setNaturalDimension] = React.useState({ naturalWidth: 0, naturalHeight: 0 });
+  const dimension = React.useMemo(() => calculateImageDimension(naturalDimension), [naturalDimension]);
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      {...rest}
+      width={dimension.width}
+      height={dimension.height}
+      layout={dimension.layout}
+      onLoadingComplete={(naturalDimension) => setNaturalDimension(naturalDimension)}
+    />
+  );
+};
 const FigureWrapper = (props) => {
   const { className, ...rest } = props;
   return <Box className={className} marginY={16} {...rest} />;
 };
 
-const MAX_PORTRAIT_HEIGHT = 720;
-const MAX_LANDSCAPE_WIDTH = 720;
-
-export const NextImage = (props) => {
-  const { src, alt, naturalWidth, naturalHeight, ...rest } = props;
-  console.log(src, alt, naturalWidth, naturalHeight);
-  if (naturalHeight > naturalWidth) {
-    if (naturalHeight <= MAX_PORTRAIT_HEIGHT) {
-      const width = naturalWidth > MAX_LANDSCAPE_WIDTH ? MAX_LANDSCAPE_WIDTH : naturalWidth;
-      return <Image src={src} alt={alt} {...rest} width={width} height={naturalHeight} layout="intrinsic" />;
-    } else {
-      // Resize using max height if larger than threshold
-      const height = MAX_PORTRAIT_HEIGHT;
-      const width = Math.floor((height / naturalHeight) * naturalWidth);
-      return <Image src={src} alt={alt} {...rest} width={width} height={height} layout="intrinsic" />;
-    }
-  } else {
-    if (naturalWidth <= MAX_LANDSCAPE_WIDTH) {
-      const height = naturalHeight > MAX_PORTRAIT_HEIGHT ? MAX_PORTRAIT_HEIGHT : naturalHeight;
-      return <Image src={src} alt={alt} {...rest} width={naturalWidth} height={height} layout="intrinsic" />;
-    } else {
-      // Resize using max width if larger than threshold
-      const width = MAX_LANDSCAPE_WIDTH;
-      const height = Math.floor((width / naturalWidth) * naturalHeight);
-      return <Image src={src} alt={alt} {...rest} width={width} height={height} layout="responsive" />;
-    }
-  }
-};
-
-export const ImageWrapper = (props) => {
-  const { className, ...rest } = props;
-  return <Box className={className} marginY={24} textAlign="center" {...rest} />;
-};
-
 export const FigureImage = (props) => {
-  const { src, alt, naturalWidth, naturalHeight, caption, ...rest } = props;
+  const { src, alt, caption, ...rest } = props;
   return (
     <FigureWrapper>
       <ImageWrapper marginY={0}>
-        <NextImage src={src} alt={alt} naturalWidth={naturalWidth} naturalHeight={naturalHeight} {...rest} />
+        <NextImage src={src} alt={alt} {...rest} />
       </ImageWrapper>
       {caption && (
         <CaptionWrapper>
