7c3449161a8dba22ce0d4bf70dcb29ecd611cb1e
Zan <nurdinizzan@gmail.com>
2022-07-13 16:50:15 +0700

    chore: remove unused file and fix logo products on mobile view

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,6 +25,7 @@ const StyledLayoutRoot = styled('div')`
 
 const LogoWrapper = styled('div')<{ isPost?: boolean }>`
   display: flex;
+  margin-top: 8px;
   align-items: ${(props) => (props.isPost ? 'center' : 'flex-start')};
   justify-content: ${(props) => (props.isPost ? 'center' : 'flex-start')};
   flex: 1;
@@ -94,8 +95,8 @@ const Layout: React.FC<ILayout> = ({ children, imageOrigin, fuseSearch }) => {
                 <UnstyledAnchor display="flex">
                   <Image
                     layout="fixed"
-                    width={60}
-                    height={40}
+                    width={200}
+                    height={60}
                     src={
                       imageOrigin === 'docs'
                         ? '/assets/images/logo-docs.svg'
@@ -122,8 +123,8 @@ const Layout: React.FC<ILayout> = ({ children, imageOrigin, fuseSearch }) => {
                 <UnstyledAnchor display="flex">
                   <Image
                     layout="fixed"
-                    width={60}
-                    height={40}
+                    width={200}
+                    height={60}
                     src={
                       imageOrigin === 'docs'
                         ? '/assets/images/logo-docs.svg'
