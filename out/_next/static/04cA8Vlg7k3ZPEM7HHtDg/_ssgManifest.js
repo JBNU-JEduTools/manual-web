9faa2e0eaf975749260c2f9d656093b51661a91f
Myeonggyu Lee <lee95292@naver.com>
2023-05-29 01:39:16 +0900

    add next.js output files

---
+++ out/_next/static/04cA8Vlg7k3ZPEM7HHtDg/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftutorials","\u002F","\u002Fbusiness-dashboard\u002F[...slug]","\u002Fkata-omnichat\u002F[...slug]","\u002Fqios\u002F[...slug]","\u002Fkata-platform\u002F[...slug]","\u002Ftutorials\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
